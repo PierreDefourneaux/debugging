379714ce721d59c4d509d21503e0d204ddb537d0
Pierre Defourneaux <pierre.defourneaux@gmail.com>
2025-10-13 14:15:37 +0200

    se limiter à pip install pylint dans le job2

--- tests_unitaires/unit_test.py
+++ tests_unitaires/unit_test.py
@@ -1,5 +1,5 @@
 import pytest
-
+import random
 # S'assurer que le bug de format ne se répète plus et soit bien adapté à celui attendu par keras
 def test_preprocessed_shape_from_random_values():
     n1 = random.randint(1, 1000)
