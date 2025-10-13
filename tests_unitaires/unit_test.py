d62b5f8207bcc8e92be138fab03a715041b7ed9c
Pierre Defourneaux <pierre.defourneaux@gmail.com>
2025-10-13 14:09:15 +0200

    renommer le test unitaire

--- tests_unitaires/unit_test.py
+++ tests_unitaires/unit_test.py
@@ -1,4 +1,9 @@
 import pytest
-# Tester le fonctionnement de l'API avec une requête GET sur la page d'accueil (route "/")
-def test_unit_1():
-    assert 100+100 == 200
+
+# S'assurer que le bug de format ne se répète plus et soit bien adapté à celui attendu par keras
+def test_preprocessed_shape_from_random_values():
+    n1 = random.randint(1, 1000)
+    n2 = random.randint(1, 1000)
+    dummy_img = Image.new("RGB", (n1, n2), color="red")
+    arr = preprocess_from_pil(dummy_img)
+    assert arr.shape == (1, 224, 224, 3)
